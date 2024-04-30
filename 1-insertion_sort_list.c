0488846bc0ca7614737733335a856eba687c5449
saadelhamidi <saadavicii123@gmail.com>
2024-04-30 10:36:38 -0700

    x

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,40 +8,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp, *prev_temp;
+listint_t *current, *temp, *prev_temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+if (!list || !(*list) || !(*list)->next)
+return;
 
-    current = (*list)->next;
+current = (*list)->next;
 
-    while (current)
-    {
-        temp = current->prev;
+while (current)
+{
+temp = current->prev;
 
-        while (temp && temp->n > current->n)
-        {
-            prev_temp = temp->prev;
+while (temp && temp->n > current->n)
+{
+prev_temp = temp->prev;
 
-            if (temp->next)
-                temp->next->prev = temp->prev;
+if (temp->next)
+temp->next->prev = temp->prev;
 
-            temp->next = current->next;
-            temp->prev = current;
+temp->next = current->next;
+temp->prev = current;
 
-            if (current->next)
-                current->next->prev = temp;
+if (current->next)
+current->next->prev = temp;
 
-            current->next = temp;
+current->next = temp;
 
-            if (prev_temp)
-                prev_temp->next = current;
-            else
-                *list = current;
+if (prev_temp)
+prev_temp->next = current;
+else
+*list = current;
 
-            print_list(*list);
-            temp = prev_temp;
-        }
-        current = current->next;
-    }
+print_list(*list);
+temp = prev_temp;
+}
+current = current->next;
+}
 }
