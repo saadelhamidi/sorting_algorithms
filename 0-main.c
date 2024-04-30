b303d6684eb2ada67e468b26139164101432c406
saadelhamidi <saadavicii123@gmail.com>
2024-04-30 09:10:40 -0700

    x

--- 0-main.c
+++ 0-main.c
@@ -12,10 +12,10 @@ int main(void)
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
-    printed_array(array, n);
+    print_array(array, n);
     printf("\n");
-    bubble_sorting(array, n);
+    bubble_sort(array, n);
     printf("\n");
-    printed_array(array, n);
+    print_array(array, n);
     return (0);
 }
