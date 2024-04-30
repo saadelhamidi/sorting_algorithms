0e012a44fbabc54c20bc7b9f35c520847a528b96
saadelhamidi <saadavicii123@gmail.com>
2024-04-30 10:28:04 -0700

    x

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,38 @@
-#include "sort.h"
+#include	"sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using
- *               the bubble sort algorithm.
- * @array: Pointer to the first element of the array
- * @size: Size of the array
- * Return: void
- */
-void bubble_sort(int *array, size_t size)
+	*	bubble_sort is a sort array elements from minimum to maximum values
+	*	@array:	array
+	*       using the Bubble sort	algorithm.
+	*	@size:	array	size
+	*	Return:	(void)
+	*/
+void	bubble_sort(int	*array,	size_t	size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t	i,	index;
+	int	tmp;
+	int	swapped;
 
-    if (size < 2 || array == NULL)
-        return;
+	if	(size	<	2	||	array	==	NULL)
+	return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
+	for	(i	=	0;	i	<	size	-	1;	i++)
+	{
+	swapped	=	0;
 
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+	for	(index	=	0;	index	<	size	-	1	-	i;	index++)
+	{
+	if	(array[index]	>	array[index	+	1])
+	{
+	tmp	=	array[index];
+	array[index]	=	array[index	+	1];
+	array[index	+	1]	=	tmp;
+	swapped	=	1;
+	print_array(array,	size);
+	}
+	}
 
-                /* Set swapped flag */
-                swapped = 1;
-
-                /* Print array after each swap */
-                print_array(array, size);
-            }
-        }
-
-        /* If no elements were swapped, array is already sorted */
-        if (!swapped)
-            break;
-    }
+	if	(!swapped)
+	break;
+	}
 }
-
